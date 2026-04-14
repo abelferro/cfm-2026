c89e769e238bd2e1beb46ae3b0ebff85ef19b0e4
abelferro <a.ferro@versal.studio>
2026-04-14 12:06:41 -0400

    Initial CMF  2026 landing

---
+++ site.config.ts
@@ -0,0 +1,17 @@
+export const landingConfig = {
+  slug: "cfm-2026",
+  siteName: "CFM 2026",
+  brandName: "WELS Foundation",
+  title: "CFM 2026",
+  description:
+    "Starter cloned from PartnerHub-Landing-Page for the CFM 2026 landing.",
+  basePath: "/lp/cfm-2026",
+  locale: "en_US",
+  keywords: [
+    "WELS Foundation",
+    "CFM 2026",
+    "landing page",
+    "Next.js",
+    "static export",
+  ],
+} as const;
